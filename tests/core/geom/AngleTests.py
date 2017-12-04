b993608692789d186f8abbb460d4e1c1896f85bb
Kirill Zorin <zorin.kirill.serg@yandex.ru>
2017-12-05 01:35:13 +0300

    Kappa AngleTests Populated for creation

--- tests/core/geom/AngleTests.py
+++ tests/core/geom/AngleTests.py
@@ -5,9 +5,21 @@ from kappa.core.geom.Angle import Angle
 
 class AngleTests(unittest.TestCase):
     def test_creation_0(self):
-        angle: Angle = Angle(0)
+        angle = Angle(0)
         self.assertAlmostEqual(angle.radians, 0)
 
+    def test_creation_pi(self):
+        angle = Angle(pi)
+        self.assertAlmostEqual(angle.radians, pi)
+
     def test_creation_2pi(self):
-        angle: Angle = Angle(2 * pi)
+        angle = Angle(2 * pi)
         self.assertAlmostEqual(angle.radians, 0)
+
+    def test_creation_3pi(self):
+        angle = Angle(3 * pi)
+        self.assertAlmostEqual(angle.radians, pi)
+
+    def test_creation_negative_pi(self):
+        angle = Angle(-pi)
+        self.assertAlmostEqual(angle.radians, pi)

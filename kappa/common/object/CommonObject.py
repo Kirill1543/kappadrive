a3eef03e9ed0492953d358c2a9c5656fdf0059d4
Kirill Zorin <zorin.kirill.serg@yandex.ru>
2017-12-18 22:18:20 +0300

    KAP-13 --- +GameObject Review

--- kappa/common/object/CommonObject.py
+++ kappa/common/object/CommonObject.py
@@ -6,6 +6,12 @@ class CommonObject(object):
         self.center = center
         self.texture = texture
 
+    def __str__(self) -> str:
+        return "{}:{}".format(self.__class__.__name__, self.center.coords)
+
+    def __repr__(self):
+        return self.__str__()
+
     def center_on(self, c_obj: __name__):
         self.move_to(c_obj.center)
 
--- kappa/common/object/GameObject.py
+++ kappa/common/object/GameObject.py
@@ -13,37 +13,34 @@ from ...logger.Logger import Logger
 class GameObject(CommonObject):
     log = Logger(__name__).get()
 
-    def __init__(self, center: Point, texture, shape: Shape, moving_style):
+    def __init__(self, center: Point, texture, shape: Shape, moving_strategy):
         CommonObject.__init__(self, center, texture)
-        self._shape: Shape = shape
-        self._m: MovingStrategy = moving_style
-
-    def __str__(self) -> str:
-        return "{}:{}".format(self.__class__.__name__, self.center.coords)
+        self.__shape: Shape = shape
+        self.__m: MovingStrategy = moving_strategy
 
     def update(self):
         pass
 
     def start_move(self, direction):
-        self._m.change_move(direction, 1)
+        self.__m.change_move(direction, 1)
 
     def stop_move(self, direction):
-        self._m.change_move(direction, -1)
+        self.__m.change_move(direction, -1)
 
     def move_offset(self, offset: Vector):
         self.center += offset
 
     def move(self, t=1):
-        self.center += self._m.get_time_offset(t)
+        self.center += self.__m.get_time_offset(t)
 
     def get_time_position(self, t=1) -> Point:
-        return self.center + self._m.get_time_offset(t)
+        return self.center + self.__m.get_time_offset(t)
 
-    def intersect(self, obj, t=0) -> bool:
+    def intersect(self, obj: __name__, t=0) -> bool:
         if self.shape.is_circle and obj.shape.is_circle:
             c: Vector = obj.center - self.center
-            v: Vector = self._m.get_time_offset(t)
-            r: float = obj.shape.radius + self._shape.radius
+            v: Vector = self.__m.get_time_offset(t)
+            r: float = obj.shape.radius + self.__shape.radius
             d = v * c - (c * c - r * r)
             GameObject.log.debug("Calculated c={}, v={}, r={}, d={}".format(c.coords, v.coords, r, d))
             if d >= 0.0:
@@ -53,13 +50,13 @@ class GameObject(CommonObject):
     def is_going_intersect(self, obj) -> bool:
         return self.intersect(obj, 1)
 
-    def move_time_to(self, obj) -> float:
+    def move_time_to(self, obj: __name__) -> float:
         GameObject.log.debug("Calculating moving time from {} to {}".format(self, obj))
         if self.shape.is_circle and obj.shape.is_circle:
             GameObject.log.debug("Both have Circle Shapes.")
             cc: Vector = self.center - obj.center
             v: Vector = self.move_vector
-            r: float = obj.shape.radius + self._shape.radius
+            r: float = obj.shape.radius + self.__shape.radius
             b = v * cc
             a = (v * v)
             c = (cc * cc - r * r)
@@ -78,31 +75,31 @@ class GameObject(CommonObject):
     def draw_shape_on(self, main_frame: Frame, center: Point):
         topleft = (int(center.x - self.width / 2), int(center.y - self.height / 2))
         GameObject.log.debug("Adding shape for {} in {}".format(self, topleft))
-        main_frame.display(self._shape.shape, topleft)
+        main_frame.display(self.__shape.shape, topleft)
 
     @property
     def width(self):
-        return self._shape.width
+        return self.__shape.width
 
     @property
     def height(self):
-        return self._shape.height
+        return self.__shape.height
 
     @property
     def x(self):
-        return self.center.x - self._shape.width // 2
+        return self.center.x - self.__shape.width // 2
 
     @x.setter
     def x(self, value):
-        self.center.x = value + self._shape.width // 2
+        self.center.x = value + self.__shape.width // 2
 
     @property
     def y(self):
-        return self.center.y - self._shape.height // 2
+        return self.center.y - self.__shape.height // 2
 
     @y.setter
     def y(self, value):
-        self.center.y = value + self._shape.height // 2
+        self.center.y = value + self.__shape.height // 2
 
     @property
     def topleft(self):
@@ -110,24 +107,24 @@ class GameObject(CommonObject):
 
     @property
     def move_vector(self):
-        return self._m.get_time_offset()
+        return self.__m.get_time_offset()
 
     @move_vector.setter
     def move_vector(self, value: Vector):
-        self._m.move_vector = value
+        self.__m.move_vector = value
 
     @property
     def is_movable(self):
-        return self._m.is_movable
+        return self.__m.is_movable
 
     @property
     def shape(self):
-        return self._shape
+        return self.__shape
 
     @property
     def speed(self):
-        return self._m.speed
+        return self.__m.speed
 
     @speed.setter
     def speed(self, value):
-        self._m.speed = value
+        self.__m.speed = value

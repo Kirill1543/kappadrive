7e35c41b5afea2c5b8b96ab3d2ce07ff5e972e2b
Kirill <zorin.kirill.serg@yandex.ru>
2018-01-08 08:00:44 +0300

    KAP-13 --- +Populated AngleTests for addition and negation +Changed private field name in Angle class

--- tests/core/geom/AngleTests.py
+++ tests/core/geom/AngleTests.py
@@ -23,3 +23,40 @@ class AngleTests(unittest.TestCase):
     def test_creation_negative_pi(self):
         angle = Angle(-pi)
         self.assertAlmostEqual(angle.radians, pi)
+
+    def test_negative_0(self):
+        angle = Angle(0)
+        self.assertAlmostEqual((-angle).radians, 0)
+
+    def test_negative_pi(self):
+        angle = Angle(pi)
+        self.assertAlmostEqual((-angle).radians, pi)
+
+    def test_negative_3(self):
+        angle = Angle(3)
+        self.assertAlmostEqual((-angle).radians, 2 * pi - 3)
+
+    def test_addition_pi_pi(self):
+        angle = Angle(pi)
+        self.assertAlmostEqual((angle + angle).radians, 0)
+
+    def test_addition_2(self):
+        angle1 = Angle(1)
+        angle2 = Angle(2)
+        self.assertAlmostEqual((angle1 + angle2).radians, 3)
+
+    def test_addition_3(self):
+        angle1 = Angle(4)
+        angle2 = Angle(6)
+        self.assertAlmostEqual((angle1 + angle2).radians, 10 - 2 * pi)
+
+    def test_addition_symmetry(self):
+        angle1 = Angle(1)
+        angle2 = Angle(2)
+        self.assertAlmostEqual((angle1 + angle2).radians, (angle2 + angle1).radians)
+
+    def test_addition_transitivity(self):
+        angle1 = Angle(1)
+        angle2 = Angle(2)
+        angle3 = Angle(3)
+        self.assertAlmostEqual(((angle1 + angle2) + angle3).radians, (angle1 + (angle2 + angle3)).radians)

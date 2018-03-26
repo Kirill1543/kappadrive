7b7960ab8ac1b7b366524bc0eb97b95b1787826d
Kirill Zorin <zorin.kirill.serg@yandex.ru>
2018-03-27 01:08:39 +0300

    Small changes: +GameMap init removed to special class from GameInstance +Added timers for Instance processes

--- kappa/core/Instance.py
+++ kappa/core/Instance.py
@@ -7,6 +7,17 @@ from ..common.screen.Screen import Screen
 from ..logger.Logger import Logger
 
 
+def timed(method):
+    def _timed(*args, **kwargs):
+        Instance.log.debug("Started {} iteration".format(method.__name__))
+        start_time = timeit.default_timer()
+        method(*args, **kwargs)
+        Instance.log.debug(
+            "Finished {} iteration. Time={}ms".format(method.__name__, (timeit.default_timer() - start_time) * 1000))
+
+    return _timed
+
+
 class Instance:
     log = Logger(__name__).get()
 
@@ -14,6 +25,8 @@ class Instance:
         self.caption: str = caption
         self.clock: Clock = None
         self.screen: Screen = None
+        self.running = True
+        self.fps = 100
 
     def init(self):
         pygame.init()
@@ -21,25 +34,32 @@ class Instance:
         pygame.display.set_caption(self.caption)
 
     def start(self):
-        while 1:
-            Instance.log.debug("Instance update iteration started")
-            start_time = timeit.default_timer()
-            self.clock.tick(100)
-            for event in pygame.event.get():
-                if not self.parse_event(event):
-                    return
+        while self.running:
+            self.tick()
 
-            self.update()
-            self.screen.display()
+    @timed
+    def tick(self):
+        self.clock.tick(self.fps)
 
-            pygame.display.flip()
-            Instance.log.debug("Instance update iteration finished:")
-            Instance.log.debug("Total execution time={}ms".format((timeit.default_timer() - start_time) * 1000))
+        self.handle_events()
+
+        self.update()
+
+        self.display()
+
+    @timed
+    def handle_events(self):
+        for event in pygame.event.get():
+            if not self.parse_event(event):
+                self.running = False
 
     def parse_event(self, event):
         return 1
 
     def update(self):
-        start_time = timeit.default_timer()
         self.screen.update()
-        Instance.log.debug("Screen updating time={}ms".format((timeit.default_timer() - start_time) * 1000))
+
+    @timed
+    def display(self):
+        self.screen.display()
+        pygame.display.flip()
